b0f99fb4c5309c753551945cea1d353909890ab8
angga150 <wijaya150906@gmail.com>
2026-05-09 12:00:51 +0700

    menambahkan view untuk admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+<x-appadmin-layout>
+    <div class="container mt-5">
+        <h1>Welcome to the Admin Dashboard</h1>
+        <p>This is the admin dashboard of our Laravel application.</p>
+    </div>
+</x-appadmin-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+}); 
+
+Route::get('/admin/users', function () {
+    return view('admin.users');
+});
